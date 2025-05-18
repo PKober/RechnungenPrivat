0efe8ec341869b64dcb3365945e9e1d8772eaa56
PKober <koberpascal@googlemail.com>
2025-05-18 09:28:41 +0200

    Projektdateien hinzufügen.

---
+++ Models/Kunde.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RechnungenPrivat.Models
+{
+    public class Kunde
+    {
+        public int Id { get; set; }
+        public string Kundenname { get; set; }
+        public string KundenAdresse { get; set; }
+    }
+}
---
+++ Views/KundenAnlegen/KundenAnlegenView.xaml.cs
@@ -0,0 +1,9 @@
+namespace RechnungenPrivat.Views.KundenAnlegen;
+
+public partial class KundenAnlegenView : ContentPage
+{
+	public KundenAnlegenView()
+	{
+		InitializeComponent();
+	}
+}
